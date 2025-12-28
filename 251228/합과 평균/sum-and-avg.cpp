5b5886ef78181d3f99214fd9058036f072539c3e
nieah78 <150982810+nieah78@users.noreply.github.com>
2025-12-28 18:17:48 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251228/합과 평균/sum-and-avg.cpp
+++ 251228/합과 평균/sum-and-avg.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
     int A, B;
     cin >> A >> B;
-    cout << A + B << " " << (double)(A + B) / 2;
+    cout << fixed;
+    cout.precision(1);
+
+    cout << A + B << " " << (double)(A + B)/2;
     return 0;
 }
