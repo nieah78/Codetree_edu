f0ab5820b2006679ea3d084615c1b370b1a2ff2d
nieah78 <150982810+nieah78@users.noreply.github.com>
2026-01-07 00:15:00 +0900

    Title: 100점 도달하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260107/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = N; i <= 100; i++){
+        if(N >= 90) cout << "A ";
+        else if(N >= 80) cout << "B ";
+        else if(N >= 70) cout << "C ";
+        else if(N >= 60) cout << "D ";
+        else cout << " F";
+
+        N++;
+    }
+    return 0;
+}
