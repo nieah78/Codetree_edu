4d58786db7d4775850cca3739c026f2ee846a073
nieah78 <150982810+nieah78@users.noreply.github.com>
2026-01-08 13:56:16 +0900

    Title: A 부터 B 까지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    while(A <= B){
+        cout << A << " ";
+
+        if(A % 2) A *= 2;
+        else A += 3;
+    }
+    return 0;
+}
