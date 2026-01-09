fb21bc181c5bb07605ebaa366b90d6495137ceab
nieah78 <150982810+nieah78@users.noreply.github.com>
2026-01-09 23:37:50 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260109/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i < N+1; i++){
+        for(int j = 1; j < N+1; j++){
+            cout << i << " * " << j << " = " << i * j;
+
+            if(j == N) continue;
+            cout << ", ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
