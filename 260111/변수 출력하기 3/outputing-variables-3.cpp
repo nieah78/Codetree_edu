fc5af2d749d2c0e3923e7306858282ea5dc61b18
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-11 15:04:33 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2;
+    char c = 'C';
+    string d = "->";
+
+    cout << a << d << b << d << c;
+    return 0;
+}
