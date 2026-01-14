046cf8069252a6a594046315fc13fbfd53a23bb0
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-14 22:14:57 +0900

    Title: 특정 조건 두 정수 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    A < B? cout<<1 : cout<<0;
+    cout << " ";
+    A == B? cout<<1 : cout<<0;
+    return 0;
+}
