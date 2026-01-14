456ec6cfef5abaa5119b9160ab02787d40e8d9fc
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-14 22:26:22 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260114/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_m, A_e, B_m, B_e;
+    cin >> A_m >> A_e;
+    cin >> B_m >> B_e;
+
+    cout << (A_m > B_m) && (A_e > B_e);
+    return 0;
+}
