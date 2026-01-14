f77f559d17e4d0609a51aee8958136dc68e7c8ab
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-14 22:23:18 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    cin >> A;
+    cin >> B >> C >> D >> E;
+
+    cout << (A>B) << "\n" << (A>C) << "\n" << (A>D) << "\n" << (A>E);
+    return 0;
+}
