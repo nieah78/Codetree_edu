8b734dd90803203f15cbb7e70d8cb4c220a8e914
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-17 00:17:10 +0900

    Title: 숫자의 증감, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/숫자의 증감/increasing-and-decreasing-numbers.cpp
+++ 260117/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -6,10 +6,7 @@ int main() {
     int N;
     cin >> C >> N;
 
-    if(C == 'A'){
-        for(int i = 1; i <= N; i++) cout << i << " ";
-    }
-
+    if(C == 'A') for(int i = 1; i <= N; i++) cout << i << " ";
     else for(int i = N; i > 0; i--) cout << i << " ";
 
     return 0;
