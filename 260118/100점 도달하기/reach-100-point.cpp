6a88b1c7f3e7e7494b6b99ccd25ea6d198b24196
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-18 03:20:07 +0900

    Title: 100점 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260118/100점 도달하기/reach-100-point.cpp
+++ 260118/100점 도달하기/reach-100-point.cpp
@@ -6,8 +6,8 @@ int main() {
     cin >> N;
 
     for(int i = N; i < 100; i++){
-        if(i >= 60) cout << (char)(74 - (i / 10)) << " ";
-        else cout << "F ";
+        if(i < 60) cout << "F ";
+        else cout << (char)(74 - (i / 10)) << " ";
     }
     cout << 'A';
     return 0;
