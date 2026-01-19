cd61461d659124158a6bf3fd3dac93cb5e8cd3e9
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-19 17:52:25 +0900

    Title: 배수의 개수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, cnt = 0;
+    
+    for(int i = 0; i < 10; i++){
+        cin >> a;
+        if(a % 2) cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
