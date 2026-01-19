28129dc7e50996d4cc7057f8faba6be3709049ed
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-19 19:32:37 +0900

    Title: 홀수만 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, a, sum = 0;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> a;
+        if(a % 2 && !(a % 3)) sum += a;
+    }
+    cout << sum;
+    return 0;
+}
