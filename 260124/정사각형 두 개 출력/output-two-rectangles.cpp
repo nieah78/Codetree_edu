af84ef107a969c230d8e794c8b38a0bf17d03e45
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-24 09:31:32 +0900

    Title: 정사각형 두 개 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < N; j++){
+            for(int k = 0; k < N; k++) cout<<'*'; cout<<"\n";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
