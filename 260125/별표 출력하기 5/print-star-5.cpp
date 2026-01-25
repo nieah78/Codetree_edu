22cd5ef1582e29c1b19b0cdcd7c9369ecc6c5dca
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-01-25 17:08:57 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260125/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = N; i > 0; i--){
+        for(int j = 0; j < i; j++){
+            for(int k = 0; k < i; k++) cout << '*';
+            cout << ' ';
+        }
+        cout << "\n";
+    }
+    return 0;
+}
