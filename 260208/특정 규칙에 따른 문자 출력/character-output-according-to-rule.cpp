67481ae9fed98e26f0a0d2321a7c2ab8fa24bb58
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-02-08 15:10:36 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260208/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 260208/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -5,7 +5,7 @@ int main() {
     int N, k;
     cin >> N;
 
-    for(int i = 1; i < N; i++){
+    for(int i = 1; i < 2*N; i++){
         if(N-i) for(int j = 0; j < N-i; j++) cout << "  ";
         k = i>N ? 2*N-i : i;
         for(; k>0; k--) cout << "@ ";
