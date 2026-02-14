ac5a5b0a2d0340a2839c9d19898fafa9bd5a3bc5
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-02-14 17:01:17 +0900

    Title: 구구단 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260214/구구단 만들기/print-multiplication-table.cpp
+++ 260214/구구단 만들기/print-multiplication-table.cpp
@@ -2,14 +2,8 @@
 using namespace std;
 
 int main() {
-    int A, B, temp;
+    int A, B;
     cin >> A >> B;
-
-    if(A > B){
-        temp = A;
-        A = B;
-        B = temp;
-    }
 
     for(int j = 1; j < 10; j++){
         for(int i = B; i >= A; i-=2){
